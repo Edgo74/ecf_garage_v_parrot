9abf80ae83140345363f0d65d05607ba82d818a5
adam <lerepairedu74@gmail.com>
2023-11-21 23:47:43 +0100

    fixation d'un bug sur la page login

--- views/Utilisateur/login.view.php
+++ views/Utilisateur/login.view.php
@@ -1,20 +1,19 @@
-
 <div class="container">
-    <div class="row">
-        <div class="col-md-6 offset-md-3">
-            <h2 class = "text-center">Page de Connexion</h2>
-            <form action="<?= URL ?>validation_login" method = "POST">
-            <input type="hidden" name="csrf_token" value="<?= $_SESSION['csrf_token'] ?>">
+   <div class="row">
+       <div class="col-md-6 offset-md-3">
+           <h2 class = "text-center">Page de Connexion</h2>
+           <form action="<?= URL ?>validation_login" method = "POST">
+           <input type="hidden" name="csrf_token" value="<?= $_SESSION['csrf_token'] ?? "" ?>">
               <div class="form-group">
                 <label for="email"><b>Email</b></label>
                 <input type="text" placeholder="Entrer votre email" name="email" required>
               </div>
               <div class="form-group">
                 <label for="password"><b>Mot de passe</b></label>
-                <input type="password" placeholder="Entrer  votre mot de passe" name="password" required>
+                <input type="password" placeholder="Entrer votre mot de passe" name="password" required>
               </div>
               <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-        </div>
-    </div>
+           </form>
+       </div>
+   </div>
 </div>
