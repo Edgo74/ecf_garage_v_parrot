b959d8c09831b19d4080b8bab965ab2a647568ef
adam <lerepairedu74@gmail.com>
2023-11-21 20:39:52 +0100

    modification fichier garage.sql

--- garage.sql
+++ garage.sql
@@ -1,7 +1,7 @@
 CREATE TABLE garage (
   numero varchar(50) NOT NULL,
   adresse varchar(250) NOT NULL
-)
+);
 
 
 CREATE TABLE avis (
@@ -10,7 +10,7 @@ CREATE TABLE avis (
   commentaire TEXT NOT NULL,
   note INT NOT NULL,
   estValide tinyint NOT NULL DEFAULT 0
-) 
+);
 
 CREATE TABLE horaires (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -20,20 +20,20 @@ CREATE TABLE horaires (
   fin_heures_PM time DEFAULT NULL,
   est_ouvert varchar(10) NOT NULL,
   jour  enum('Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi','Dimanche')
-) 
+);
 
 CREATE TABLE services (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   titre varchar(100) NOT NULL,
   description TEXT NULL
-)
+);
 
 CREATE TABLE utilisateur (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   password varchar(100) NOT NULL,
   mail varchar(100) NOT NULL,
   role varchar(50) NOT NULL DEFAULT 'employe'
-)
+);
 
 CREATE TABLE voitures (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -43,16 +43,20 @@ CREATE TABLE voitures (
   kilometre INT NOT NULL,
   price float NOT NULL,
   image varchar(100) NOT NULL
-)
+);
 
 -- Insertion de donnees fictives dans la base de données
+
+INSERT INTO utilisateur (password, mail, role) VALUES
+('$2y$10$/pPjdYpzR0Q3Rgv81KyA0.njs.Ik1uAkMzoyeSgQhVc8x3kYldSG6', 'admin@mail.com', 'administrateur');
 
 INSERT INTO avis (nom, commentaire, note, estValide) VALUES
 ('Joseph', 'Super', 5, 1),
 ('John', 'tres bien ', 5, 1),
-('Adam', 'Jaime bien.', 4),
+('Adam', 'Jaime bien.', 4,0),
 ('Milo', 'moyen', 3, 1),
-('Tina', 'Super!', 4);
+('Tina', 'Super!', 4,0);
+
 
 
 INSERT INTO horaires (debut_heures_AM, fin_heures_AM, debut_heures_PM, fin_heures_PM, est_ouvert, jour) VALUES
@@ -69,4 +73,12 @@ INSERT INTO services (titre, description) VALUES
 ('Réparations mécaniques', 'Réparation et diagnostic des problèmes électriques, remplacement de la batterie.'),
 ('Réparation des vitres', 'Réparation ou remplacement des vitres et pare-brise.'),
 ('Services de carrosserie', 'Réparation des dommages de carrosserie, peinture, polissage.'),
-('Pneus et roues', 'Rotation des pneus, équilibrage, alignement, remplacement des pneus.'),
+('Pneus et roues', 'Rotation des pneus, équilibrage, alignement, remplacement des pneus.');
+
+INSERT INTO voitures (titre, year, carburant, kilometre, price, image) VALUES
+('Renault', '2002', 'diesel', 50000, 15000, '17424_voiture2.jpg'),
+('Ferrari', '2009', 'diesel', 33000, 42000, '20511_voiture2.jpg'),
+('Mercedes', '2000', 'diesel', 220000, 16000, '34235_voiture.jpg'),
+('Alpha Romeo', '2009', 'diesel', 180000, 42000, '82328_voiture2.jpg');
+
+INSERT INTO garage(numero, adresse) VALUES("Toulouse", "061357896");
