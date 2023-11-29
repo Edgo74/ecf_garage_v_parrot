047b799a8baa60955edd45d4024a54e2ba94b8db
adam <lerepairedu74@gmail.com>
2023-11-29 10:57:06 +0100

    bootstrap js cdn

--- views/Commons/template.php
+++ views/Commons/template.php
@@ -35,6 +35,7 @@
     <?= $page_content ?>
     <?php require("views/Commons/footer.php") ?>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <?php if(!empty($page_javascript)): ?>
         <script src = "<?=URL ?>public/JavaScript/<?=$page_javascript?>"></script>
     <?php endif;?>
