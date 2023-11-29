de713aa3474d8f2d267c8ae878ff5a7d49434517
adam <lerepairedu74@gmail.com>
2023-11-29 08:58:42 +0100

    redeploy databse

--- models/Model.class.php
+++ models/Model.class.php
@@ -4,7 +4,14 @@ abstract class Model{
     private static $pdo;
 
     private static function setBdd(){
-        self::$pdo = new PDO("mysql://mltywlhbnv995tic:khrdi7hr71f6s25s@l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/nw3w1xy3zgeai98w");
+        $url = parse_url(getenv("JAWSDB_URL"));
+        $host = $url["host"];
+        $db  = substr($url["path"], 1);
+        $user = $url["user"];
+        $pass = $url["pass"];
+        $port = $url["port"];
+
+        self::$pdo = new PDO("mysql:host=$host;dbname=$db;port=$port;charset=utf8", $user, $pass);
         self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
