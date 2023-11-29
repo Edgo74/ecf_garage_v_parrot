631619e354e8fa2e16255c336750c188b8714f7d
adam <lerepairedu74@gmail.com>
2023-11-29 10:46:55 +0100

    bdd

--- models/Model.class.php
+++ models/Model.class.php
@@ -4,7 +4,14 @@ abstract class Model{
     private static $pdo;
 
     private static function setBdd(){
-        self::$pdo = new PDO("mysql:host=localhost;dbname=garage;charset=utf8", "root", "");
+        $url = parse_url(getenv("JAWSDB_URL"));
+        $host = $url["host"];
+        $db  = substr($url["path"], 1);
+        $user = $url["user"];
+        $pass = $url["pass"];
+        $port = $url["port"];
+
+        self::$pdo = new PDO("mysql:host=$host;dbname=$db;port=$port;charset=utf8", $user, $pass);
         self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
