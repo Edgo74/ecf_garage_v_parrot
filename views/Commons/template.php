68cb829549966c09410d428b5d49d5b1aa56d677
adam <lerepairedu74@gmail.com>
2023-11-29 14:04:01 +0100

    optimisation

--- views/Commons/template.php
+++ views/Commons/template.php
@@ -36,6 +36,7 @@
     <?php require("views/Commons/footer.php") ?>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <?php if(!empty($page_javascript)): ?>
+        <script src="<?=  URL ?>public/bootstrap/js/bootstrap.js"></script>
         <script src = "<?=URL ?>public/JavaScript/<?=$page_javascript?>"></script>
     <?php endif;?>
 </body>
