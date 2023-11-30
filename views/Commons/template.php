407a1f5a9ff0bc92193757acad0a548532aa6c6b
adam <lerepairedu74@gmail.com>
2023-11-30 13:56:21 +0100

    analytics tracking

--- views/Commons/template.php
+++ views/Commons/template.php
@@ -1,42 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="<?= $page_description?>">
+    <meta name="description" content="<?= $page_description ?>">
     <?php if (!isset($_SESSION['csrf_token'])) {
-    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));}
+        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+    }
     ?>
     <meta name="csrf-token" content="<?= $_SESSION['csrf_token'] ?>" />
-    <title><?= $page_title?></title>
+    <title><?= $page_title ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= URL ?>public/CSS/main.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
-    <?php if(!empty($page_css)) : ?>
-        <link rel="stylesheet" href="<?= URL ?>public/CSS/<?=$page_css?>">
+    <?php if (!empty($page_css)) : ?>
+        <link rel="stylesheet" href="<?= URL ?>public/CSS/<?= $page_css ?>">
     <?php endif; ?>
 </head>
+
 <body class="d-flex flex-column vh-100">
     <?php require("views/Commons/header.php") ?>
 
-    <?php 
-            if(!empty($_SESSION['alert'])) {
-                foreach($_SESSION['alert'] as $alert){
-                    echo "<div class='alert ".  $alert['type'] ."' role='alert'>
-                        ".$alert['message']."
+    <?php
+    if (!empty($_SESSION['alert'])) {
+        foreach ($_SESSION['alert'] as $alert) {
+            echo "<div class='alert " .  $alert['type'] . "' role='alert'>
+                        " . $alert['message'] . "
                     </div>";
-                }
-                unset($_SESSION['alert']);
-            }
-        ?>
+        }
+        unset($_SESSION['alert']);
+    }
+    ?>
 
     <?= $page_content ?>
     <?php require("views/Commons/footer.php") ?>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <?php if(!empty($page_javascript)): ?>
-        <script src = "<?=URL ?>public/JavaScript/<?=$page_javascript?>"></script>
-    <?php endif;?>
+    <?php if (!empty($page_javascript)) : ?>
+        <script src="<?= URL ?>public/JavaScript/<?= $page_javascript ?>"></script>
+    <?php endif; ?>
+    <!-- 100% privacy-first analytics -->
+    <script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+    <noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt="" referrerpolicy="no-referrer-when-downgrade" /></noscript>
 </body>
+
 </html>
