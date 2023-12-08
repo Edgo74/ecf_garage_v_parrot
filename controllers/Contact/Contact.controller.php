2b36acc8c735b2a6f3fdae31488e8fbcc03947de
adam <lerepairedu74@gmail.com>
2023-12-08 11:21:27 +0100

    adding composer and phpmailer

--- controllers/Contact/Contact.controller.php
+++ controllers/Contact/Contact.controller.php
@@ -1,9 +1,17 @@
-<?php 
+<?php
 require_once("controllers/Maincontroller.controller.php");
+require 'PHPMailer/PHPMailerAutoload.php';
+require 'vendor/autoload.php';
 
-class ContactController extends MainController{
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-    public function contact(){
+class ContactController extends MainController
+{
+
+    public function contact()
+    {
         $data_page = [
             "page_description" => "page de contact ",
             "page_title" => "page de Contact",
@@ -14,19 +22,53 @@ class ContactController extends MainController{
         $this->genererPage($data_page);
     }
 
-    public function validationFormulaireContact($nom, $prenom, $email, $message){
+
+
+
+    public function validationFormulaireContact($nom, $prenom, $email, $message)
+    {
         if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
             Toolbox::ajouterMessageAlerte("vous n'avez pas le droit d envoyer ce formulaire", Toolbox::COULEUR_ROUGE);
-        }else{
-            $destinataire = "adambayar1357@gmail.com";
-            $mail = mail($destinataire, "-". $nom . "- " . $prenom, "Mail :". $email . "Message :" .$message);
-            if($mail){
-                Toolbox::ajouterMessageAlerte("Message Envoyé", Toolbox::COULEUR_VERTE);
-                header("location:". URL. "accueil");
-            }else{
+        } else {
+            $mail = new PHPMailer;
+
+            $mail = new PHPMailer;
+
+            $mail->isSMTP();
+            $mail->Host = 'sandbox.smtp.mailtrap.io'; // ou 'live.smtp.mailtrap.io' pour le serveur en direct
+            $mail->SMTPAuth = true;
+            $mail->Username = getenv('MAILTRAP_USERNAME');
+            $mail->Password = getenv('MAILTRAP_PASSWORD');
+            //$mail->SMTPSecure = 'tls';
+            $mail->Port = 2525;
+
+            $mail->setFrom('adambayar1357@gmail.com',  $nom . "-" . $prenom);
+            $mail->addAddress('lerepairedu74@gmail.com', 'lerepaire-2');
+            $mail->addReplyTo('adambayar1357@gmail.com', 'lerepaire-3');
+
+            $mail->isHTML(true);
+
+            $mail->Subject = 'Here is the subject';
+            $mail->Body = $message;
+            $mail->AltBody = 'Mail : ' . $email . PHP_EOL;
+            'the body in plain text for non-HTML mail clients';
+
+            if (!$mail->send()) {
                 Toolbox::ajouterMessageAlerte("Un probleme est survenu veuillez réessayer", Toolbox::COULEUR_ROUGE);
-                header("location:". URL. "contact");
+                header("location:" . URL . "contact");
+            } else {
+                Toolbox::ajouterMessageAlerte("Message Envoyé", Toolbox::COULEUR_VERTE);
+                header("location:" . URL . "accueil");
             }
-        }    
+            // $destinataire = "adambayar1357@gmail.com";
+            // $mail = mail($destinataire, "-" . $nom . "- " . $prenom, "Mail :" . $email . "Message :" . $message);
+            // if ($mail) {
+            //     Toolbox::ajouterMessageAlerte("Message Envoyé", Toolbox::COULEUR_VERTE);
+            //     header("location:" . URL . "accueil");
+            // } else {
+            //     Toolbox::ajouterMessageAlerte("Un probleme est survenu veuillez réessayer", Toolbox::COULEUR_ROUGE);
+            //     header("location:" . URL . "contact");
+            // }
+        }
     }
-} 
+}
