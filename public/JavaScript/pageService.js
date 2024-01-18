830cae03182117d1cfbed4227803086948802bc8
adam <lerepairedu74@gmail.com>
2024-01-18 19:51:22 +0100

    bug fixing

--- public/JavaScript/pageService.js
+++ public/JavaScript/pageService.js
@@ -1,33 +1,34 @@
+if (document.readyState === "complete" || document.readyState === "interactive") {
+    let titre = document.querySelector("#Titre");
+    let description = document.querySelector("#description");
 
-let titre = document.querySelector("#Titre");
-let description = document.querySelector("#description");
+    function updateButtonsState() {
+      if (
+          titre.value == "" ||
+          description.value == "" 
+      ) {
+          document.querySelector("#validateButton").disabled = true;
+          document.querySelector("#deleteButton").classList.add("btn-disabled");
+          document.querySelector("#deleteButton").addEventListener("click", function (event) {
+              event.preventDefault();
+          });
+      } else {
+          document.querySelector("#validateButton").disabled = false;
+          document.querySelector("#deleteButton").classList.remove("btn-disabled");
+          document.querySelector("#deleteButton").addEventListener("click", function (event) {
+              confirm('voulez-vous vraiment supprimer ce service ? ');
+          });
+      }
+  }
 
 
-function updateButtonsState() {
-    if (
-        titre.value == "" ||
-        description.value == "" 
-    ) {
-        document.querySelector("#validateButton").disabled = true;
-        document.querySelector("#deleteButton").classList.add("btn-disabled");
-        document.querySelector("#deleteButton").addEventListener("click", function (event) {
-            event.preventDefault();
-        });
-    } else {
-        document.querySelector("#validateButton").disabled = false;
-        document.querySelector("#deleteButton").classList.remove("btn-disabled");
-        document.querySelector("#deleteButton").addEventListener("click", function (event) {
-            confirm('voulez-vous vraiment supprimer ce service ? ');
-        });
-    }
+  updateButtonsState();
+
+  // Add event listeners to update button state on input change
+  titre.addEventListener("input", updateButtonsState);
+  description.addEventListener("input", updateButtonsState);
+  
 }
-
-updateButtonsState();
-
-// Add event listeners to update button state on input change
-titre.addEventListener("input", updateButtonsState);
-description.addEventListener("input", updateButtonsState);
-
 
 
 var baseUrl = "https://app-ecf-garage-3d639a49eac3.herokuapp.com/";
