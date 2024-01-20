61ae75d721012c4583328bed82b5f5a77e9a65ee
adam <lerepairedu74@gmail.com>
2024-01-20 12:08:37 +0100

    image fix

--- controllers/Toolbox.class.php
+++ controllers/Toolbox.class.php
@@ -1,38 +1,39 @@
 <?php
-class Toolbox {
+class Toolbox
+{
     public const COULEUR_ROUGE = "alert-danger";
     public const COULEUR_ORANGE = "alert-warning";
     public const COULEUR_VERTE = "alert-success";
 
-    public static function ajouterMessageAlerte($message,$type){
-        $_SESSION['alert'][]=[
+    public static function ajouterMessageAlerte($message, $type)
+    {
+        $_SESSION['alert'][] = [
             "message" => $message,
             "type" => $type
         ];
     }
 
-    public static function ajoutImage($file, $dir){
-        if(!isset($file['name']) || empty($file['name']))
+    public static function ajoutImage($file, $dir)
+    {
+        if (!isset($file['name']) || empty($file['name']))
             throw new Exception("Vous devez indiquer une image");
-    
-        if(!file_exists($dir)) mkdir($dir,0777);
-    
-        $extension = strtolower(pathinfo($file['name'],PATHINFO_EXTENSION));
-        $random = rand(0,99999);
-        $target_file = $dir.$random."_".$file['name'];
-        
-        if(!getimagesize($file["tmp_name"]))
+
+        if (!file_exists($dir)) mkdir($dir, 0777);
+
+        $extension = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
+        $random = rand(0, 99999);
+        $target_file = $dir . $random . "_" . $file['name'];
+
+        if (!getimagesize($file["tmp_name"]))
             throw new Exception("Le fichier n'est pas une image");
-        if($extension !== "jpg" && $extension !== "jpeg" && $extension !== "png" && $extension !== "gif")
+        if ($extension !== "jpg" && $extension !== "jpeg" && $extension !== "png" && $extension !== "gif")
             throw new Exception("L'extension du fichier n'est pas reconnu");
-        if(file_exists($target_file))
+        if (file_exists($target_file))
             throw new Exception("Le fichier existe déjà");
-        if($file['size'] > 500000)
+        if ($file['size'] > 50000000)
             throw new Exception("Le fichier est trop gros");
-        if(!move_uploaded_file($file['tmp_name'], $target_file))
+        if (!move_uploaded_file($file['tmp_name'], $target_file))
             throw new Exception("l'ajout de l'image n'a pas fonctionné");
-        else return ($random."_".$file['name']);
+        else return ($random . "_" . $file['name']);
     }
 }
-
-
