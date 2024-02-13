e614b142afaa9cf35d3b854f62281cd15d06321a
adam <lerepairedu74@gmail.com>
2024-02-13 09:45:30 +0100

    using fetch

--- public/JavaScript/pageService.js
+++ public/JavaScript/pageService.js
@@ -1,44 +1,92 @@
+
 var baseUrl = "https://app-ecf-garage-3d639a49eac3.herokuapp.com/";
 
- document.addEventListener("DOMContentLoaded", function() {
-     document.querySelector('select[name="serviceId"]').addEventListener("change", function() {
-         var serviceId = this.value;
-         var csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+document.addEventListener("DOMContentLoaded", function() {
+  var serviceSelect = document.querySelector('select[name="serviceId"]');
+  serviceSelect.addEventListener('change', function() {
+    var serviceId = this.value;
+    var csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    myHeaders.append("X-CSRF-TOKEN", csrfToken);
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: new URLSearchParams({
+        csrf_token: csrfToken,
+        serviceId: serviceId
+      }).toString(),
+      redirect: 'follow'
+    };
+
+    fetch(baseUrl + 'Services/modifier_supprimer_service', requestOptions)
+      .then(response => response.text())
+      .then(result => {
+        var serviceData = JSON.parse(result);
+        if (serviceData.error) {
+          alert(serviceData.error);
+        } else {
+          document.querySelector('input[name="Titre"]').value = serviceData.titre;
+          document.querySelector('textarea[name="description"]').value = serviceData.description;
+          document.querySelector('input[name="identifiant"]').value = serviceData.id;
+          document.getElementById('deleteButton').setAttribute('href', baseUrl + 'Services/supprimerService/' + serviceData.id);
+        }
+      })
+      .catch(error => console.log('error', error));
+  });
+});
+
+
+
+
+
+
+
+
+
+// var baseUrl = "https://app-ecf-garage-3d639a49eac3.herokuapp.com/";
+
+//  document.addEventListener("DOMContentLoaded", function() {
+//      document.querySelector('select[name="serviceId"]').addEventListener("change", function() {
+//          var serviceId = this.value;
+//          var csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
          
-         fetch(baseUrl + 'Services/modifier_supprimer_service', {
-             method: 'POST',
-             headers: {
-                 'X-CSRF-TOKEN': csrfToken
-             },
-             body: JSON.stringify({
-                 csrf_token: csrfToken,
-                 serviceId: serviceId
-             })
-         })
-         .then(response => {
-             if (!response.ok) {
-                 throw new Error('Network response was not ok');
-             }
-             return response.json();
-         })
-         .then(serviceData => {
-             if (serviceData.error) {
-                 alert(serviceData.error);
-             } else {
-                 document.querySelector('input[name="Titre"]').value = serviceData.titre;
-                 document.querySelector('textarea[name="description"]').value = serviceData.description;
-                 document.querySelector('input[name="identifiant"]').value = serviceData.id;
-                 document.getElementById('deleteButton').setAttribute('href', baseUrl + 'Services/supprimerService/' + serviceData.id);
-             }
-         })
-         .catch(error => {
-             console.error('There was a problem with the fetch operation:', error);
-         });
-     });
- });
+//          fetch(baseUrl + 'Services/modifier_supprimer_service', {
+//              method: 'POST',
+//              headers: {
+//                  'X-CSRF-TOKEN': csrfToken
+//              },
+//              body: JSON.stringify({
+//                  csrf_token: csrfToken,
+//                  serviceId: serviceId
+//              })
+//          })
+//          .then(response => {
+//              if (!response.ok) {
+//                  throw new Error('Network response was not ok');
+//              }
+//              return response.json();
+//          })
+//          .then(serviceData => {
+//              if (serviceData.error) {
+//                  alert(serviceData.error);
+//              } else {
+//                  document.querySelector('input[name="Titre"]').value = serviceData.titre;
+//                  document.querySelector('textarea[name="description"]').value = serviceData.description;
+//                  document.querySelector('input[name="identifiant"]').value = serviceData.id;
+//                  document.getElementById('deleteButton').setAttribute('href', baseUrl + 'Services/supprimerService/' + serviceData.id);
+//              }
+//          })
+//          .catch(error => {
+//              console.error('There was a problem with the fetch operation:', error);
+//          });
+//      });
+//  });
  
 
-
+//  var baseUrl = "https://app-ecf-garage-3d639a49eac3.herokuapp.com/";
 
 //  $(document).ready(function() {
 //   $('select[name="serviceId"]').change(function() {
@@ -71,3 +119,6 @@ var baseUrl = "https://app-ecf-garage-3d639a49eac3.herokuapp.com/";
 //     });
 //   });
 // });
+
+
+
