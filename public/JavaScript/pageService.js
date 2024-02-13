4b79555be17d6f5b09f63ff17cdf31a84d6823e6
adam <lerepairedu74@gmail.com>
2024-02-13 09:38:58 +0100

    content type

--- public/JavaScript/pageService.js
+++ public/JavaScript/pageService.js
@@ -8,7 +8,6 @@ var baseUrl = "https://app-ecf-garage-3d639a49eac3.herokuapp.com/";
          fetch(baseUrl + 'Services/modifier_supprimer_service', {
              method: 'POST',
              headers: {
-                 'Content-Type': 'application/x-www-form-urlencoded',
                  'X-CSRF-TOKEN': csrfToken
              },
              body: JSON.stringify({
@@ -41,3 +40,34 @@ var baseUrl = "https://app-ecf-garage-3d639a49eac3.herokuapp.com/";
 
 
 
+//  $(document).ready(function() {
+//   $('select[name="serviceId"]').change(function() {
+//     var serviceId = $(this).val();
+    
+//     $.ajaxSetup({
+//       headers: {
+//           'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+//       }
+//    });
+   
+//     $.ajax({
+//       url: baseUrl + 'Services/modifier_supprimer_service',
+//       method: 'post',
+//       data: {
+//         csrf_token: $('meta[name="csrf-token"]').attr('content'),
+//         serviceId: serviceId
+//       },
+//       success: function(result) {
+//           var serviceData = JSON.parse(result);
+//           if (serviceData.error) {
+//               alert(serviceData.error);
+//           } else {
+//               $('input[name="Titre"]').val(serviceData.titre);
+//               $('textarea[name="description"]').val(serviceData.description);
+//               $('input[name="identifiant"]').val(serviceData.id);
+//               $('#deleteButton').attr('href', baseUrl + 'Services/supprimerService/' + serviceData.id);
+//           }
+//       }
+//     });
+//   });
+// });
