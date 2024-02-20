cac78446c62a4e13761884469543ea609a399c36
adam <lerepairedu74@gmail.com>
2024-02-20 09:23:43 +0100

    add error image

--- views/erreur.view.php
+++ views/erreur.view.php
@@ -5,7 +5,7 @@
                 <div class="row justify-content-center">
                     <div class="col-lg-6">
                         <div class="text-center mt-4">
-                            <img class="mb-4 img-error" src="assets/img/error-404-monochrome.svg" />
+                            <img class="mb-4 img-error" src="<?= URL ?>public/assets/images/error404.svg" />
                             <p class="lead"><?= $msg ?></p>
                             <a href="<?= URL ?>accueil">
                                 <i class="fas fa-arrow-left me-1"></i>
