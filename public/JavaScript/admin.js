eefa8c97dd365127c12166200857d85d94e997af
adam <lerepairedu74@gmail.com>
2024-04-04 23:14:57 +0200

    add admin page js

---
+++ public/JavaScript/admin.js
@@ -0,0 +1,11 @@
+
+
+const accordionBtns = document.querySelectorAll('.accordion-btn');
+
+accordionBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const content = btn.nextElementSibling;
+        content.style.display = content.style.display === 'block' ? 'none' : 'block';
+    });
+});
+
